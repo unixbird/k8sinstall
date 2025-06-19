69d9f8a484eca6db4affcccb6c711678f21aad79
Zach <unixbird@racky.systems>
2025-06-19 14:18:07 -0500

    Add files via upload
    Main install script that I got from techiescamp but modified to fit my environment better

---
+++ kubeinstall.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+#
+# Common setup for all servers (Control Plane and Nodes)
+
+set -euxo pipefail
+
+# Kubernetes Variable Declaration
+read -e -p "what kubernetes version do you want to install (v1.30)?: " KUBERNETES_VERSION
+read -e -p "what version of CRI-O do you want to install (v1.30)?: " CRIO_VERSION
+
+# Disable swap and persist after reboot if swap exists
+sudo swapoff -a
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+
+sudo apt-get update -y
+sudo apt install bash-completion
+
+# Create the .conf file to load the modules at bootup
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+
+# Install CRI-O Runtime
+sudo apt-get update -y
+sudo apt-get install -y software-properties-common curl apt-transport-https ca-certificates
+
+curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/deb/Release.key |
+    gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/deb/ /" |
+    tee /etc/apt/sources.list.d/cri-o.list
+
+sudo apt-get update -y
+sudo apt-get install -y cri-o
+
+sudo systemctl daemon-reload
+sudo systemctl enable crio --now
+sudo systemctl start crio.service
+
+echo "CRI runtime installed successfully"
+
+# Install kubelet, kubectl, and kubeadm
+curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/Release.key |
+    gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/ /" |
+    tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update -y
+set -x #echo on
+sudo apt-cache madison kubelet
+read -e -p "What specific patch version do you want?: " KUBERNETES_INSTALL_VERSION
+sudo apt-get install -y kubelet="$KUBERNETES_INSTALL_VERSION" kubectl="$KUBERNETES_INSTALL_VERSION" kubeadm="$KUBERNETES_INSTALL_VERSION"
+
+# Prevent automatic updates for kubelet, kubeadm, and kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo apt-get update -y
+
+read -e -p "what is the node ip?: " local_ip
+
+# Write the local IP address to the kubelet default configuration file
+cat > /etc/default/kubelet << EOF
+KUBELET_EXTRA_ARGS=--node-ip=$local_ip
+EOF
+
+set -euxo pipefail
+
+# If you need public access to API server using the servers Public IP adress, change PUBLIC_IP_ACCESS to true.
+
+read -e -p "what do you want the nodename to be? NO UPPERCASE: " NODENAME
+read -e -p "what do you want the pod cidr to be? (192.168.0.0/16): " POD_CIDR
+
+# Pull required images
+
+sudo kubeadm config images pull
+
+MASTER_PUBLIC_IP=$(curl ifconfig.me && echo "")
+sudo kubeadm init --control-plane-endpoint="$MASTER_PUBLIC_IP" --apiserver-cert-extra-sans="$MASTER_PUBLIC_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
+
+
+# Configure kubeconfig
+
+mkdir -p "$HOME"/.kube
+sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
+sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
+
+# Taint the node to accept worker jobs
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+
+# Install Calico operator for CNI
+
+kubectl apply --server-side --force-conflicts -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.0/manifests/operator-crds.yaml
+
+kubectl apply --server-side --force-conflicts -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.0/manifests/tigera-operator.yaml
+
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.30.0/manifests/custom-resources.yaml -O && kubectl create -f custom-resources.yaml
+
+# install helm
+
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+# deploy metal-lb
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.15.2/config/manifests/metallb-native.yaml
+
+kubectl apply -f metalconfig.yml
+
+# deploy ingress-nginx
+helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace
